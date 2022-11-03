d7f24110af7a096f04486c39e656dd304015935d
Jonathan Fernandez-Mertanen <jonathan.fernandez-mertanen@efecte.com>
2022-11-03 17:32:03 +0200

    Update to Angular 14

--- projects/ng-hotkeys/src/lib/ng-hotkeys.module.ts
+++ projects/ng-hotkeys/src/lib/ng-hotkeys.module.ts
@@ -10,7 +10,6 @@ import { CommonModule } from "@angular/common";
 
 @NgModule({
     imports: [CommonModule],
-    entryComponents: [NgHotkeysHelpComponent],
     declarations: [
         NgHotkeysComponent,
         NgHotkeysDirective,
